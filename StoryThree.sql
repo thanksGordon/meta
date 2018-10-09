35fdcc83d25b9d8be9b834a0469f2703b368720d
Ryan Connolly <ryanco@kainos.com>
2018-10-09 09:09:16 +0100

    Added story three

---
+++ StoryThree.sql
@@ -0,0 +1,19 @@
+DESC Employee;
+DESC EmpSales;
+Select *
+from Department;
+Delimiter //
+ 
+/* this procedure assumes the reocrd is being created an update procedure is seperate*/
+CREATE procedure NewSalesEmp(IN Fnm Varchar(30),Lnm Varchar(30),NatIN Char(9) ,IB varchar(34) ,Sales float,Commission float)
+begin
+/* inserting intial values */
+Insert into Employee(fname,lname,NIN,IBAN,DepartmentID) values (Fnm,Lnm,NatIN,IB,3); /* Error with unkown cause */
+/* trying to find the new created record and obtain its ID */
+@Ident = (select EmployeeID
+from Employee
+where NIN = NatIN and IBAN = IB);
+/* using the newly created variable to mimic the record and insert the correct values */
+insert into EmpSales(EmpID,Month,TotalSales,ComRate) values (Ident,Now(),Sales,Commission);
+ end 
+Delimiter //
